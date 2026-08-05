5676d942aeaac8de65948812e048e6a807a55da1
thebigdeal66-ai <thebigdeal66@gmail.com>
2026-08-05 02:06:33 -0400

    feat: start location tracking from app shell

--- partysafari/src/app/layout.tsx
+++ partysafari/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/NavBar";
 import GlobalRuntimeKillSwitch from "../components/GlobalRuntimeKillSwitch";
+import GlobalUserLocationTracker from "../components/GlobalUserLocationTracker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <GlobalRuntimeKillSwitch />
+        <GlobalUserLocationTracker />
         <NavBar />
         {children}
       </body>
